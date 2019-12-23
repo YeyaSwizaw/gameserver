07939bb6df73ce5e6e5a73ad1d95bbfc142195f6
Samuel Sleight <samuel.sleight@gmail.com>
2019-12-23 12:54:46 +0000

    Update lobby and tidy up some unneeded concurrency

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -6,7 +6,7 @@ use std::{
 };
 
 use shared::proto;
-use lobby::{Lobby, Event, EventKind};
+use lobby::{Lobby, LobbySender, Event, EventKind};
 use vec_map::{Entry, VecMap};
 
 pub use shared::game::Game;
@@ -18,30 +18,28 @@ enum Connection {
 }
 
 pub struct GameServer<G: Game + 'static> {
-    lobby: Arc<Mutex<Lobby<proto::Client<G>>>>,
-    connections: Arc<Mutex<VecMap<Connection>>>,
-    players: Players<G>
+    sender: LobbySender,
+    connections: VecMap<Connection>,
+    players: Players<G>,
 }
 
 impl<G: Game + 'static> GameServer<G> {
-    fn new(lobby: Arc<Mutex<Lobby<proto::Client<G>>>>) -> Self {
-        let players = Players::new();
-
+    fn new(sender: LobbySender) -> Self {
         GameServer {
-            lobby,
-            connections: Arc::new(Mutex::new(VecMap::new())),
-            players,
+            sender,
+            connections: VecMap::new(),
+            players: Players::new(),
         }
     }
 
     fn handle_event(&mut self, event: Event<proto::Client<G>>) {
         match event.event {
             EventKind::DataReceived(proto::Client::ChatMessage(text)) => {
-                self.lobby.lock().unwrap().send_to_except(event.from, proto::Server::ChatMessage::<G>(event.from, text.trim_end().into())).unwrap();
+                self.sender.send_to_except(event.from, proto::Server::ChatMessage::<G>(event.from, text.trim_end().into())).unwrap();
             },
 
             EventKind::DataReceived(proto::Client::PlayerUpdate(player)) => {
-                if let Entry::Occupied(ref mut entry) = self.connections.lock().unwrap().entry(event.from) {
+                if let Entry::Occupied(ref mut entry) = self.connections.entry(event.from) {
                     let player_clone = player.clone();
                     let mut players = self.players.clone();
 
@@ -56,27 +54,25 @@ impl<G: Game + 'static> GameServer<G> {
                             }
                         });
 
-                    self.lobby.lock().unwrap().send(proto::Server::PlayerUpdate::<G>(event.from, player_clone)).unwrap();
+                    self.sender.send(proto::Server::PlayerUpdate::<G>(event.from, player_clone)).unwrap();
                 }
             }
 
             EventKind::ConnectionReceived(addr) => {
-                self.connections.lock().unwrap().insert(event.from, Connection::Uninitialised);
+                self.connections.insert(event.from, Connection::Uninitialised);
 
                 {
-                    let lobby = self.lobby.lock().unwrap();
-
-                    lobby.send_to_except(event.from, proto::Server::Connection::<G>(event.from, addr)).unwrap();
+                    self.sender.send_to_except(event.from, proto::Server::Connection::<G>(event.from, addr)).unwrap();
 
                     self.players.with(
-                        |index, player| lobby.send_to(event.from, proto::Server::PlayerUpdate::<G>(index, player.clone())).unwrap()
+                        |index, player| self.sender.send_to(event.from, proto::Server::PlayerUpdate::<G>(index, player.clone())).unwrap()
                     )
                 }
             },
 
             EventKind::ConnectionLost(_) => {
-                self.connections.lock().unwrap().remove(event.from);
-                self.lobby.lock().unwrap().send(proto::Server::ConnectionLost::<G>(event.from)).unwrap();
+                self.connections.remove(event.from);
+                self.sender.send(proto::Server::ConnectionLost::<G>(event.from)).unwrap();
             },
 
             EventKind::DataError(err) => {
@@ -86,24 +82,16 @@ impl<G: Game + 'static> GameServer<G> {
     }
 
     pub fn spawn<A: ToSocketAddrs>(addr: A) -> Result<Arc<Mutex<Self>>> {
-        let lobby = Arc::new(Mutex::new(Lobby::spawn(addr)?));
-        let thread_lobby = lobby.clone();
+        let (sender, receiver) = Lobby::<proto::Client<G>>::spawn(addr).unwrap();
 
-        let server = Arc::new(Mutex::new(GameServer::new(lobby)));
+        let server = Arc::new(Mutex::new(GameServer::new(sender)));
         let thread_server = server.clone();
 
         thread::spawn(move || {
-            let lobby = thread_lobby;
             let server = thread_server;
 
             loop {
-                let events: Vec<_> = {
-                    let lock = lobby.lock().unwrap();
-                    let iter = lock.events();
-                    iter.collect()
-                };
-
-                for event in events {
+                for event in receiver.events() {
                     server.lock().unwrap().handle_event(event);
                 }
             }
